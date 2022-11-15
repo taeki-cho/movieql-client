e8676a2fb50c23ab86a9dcf272352f7d1788e58e
taeki-cho <taegi81@gmail.com>
2022-11-15 16:51:10 +0900

    end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movie from "./routes/Movie";
+import Movies from "./routes/Movies";
 
-function App() {
-  return <div></div>;
+export default function App() {
+  return ( 
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Movies />} />
+      <Route path="/movie/:id" element={<Movie />} />
+    </Routes>
+  </BrowserRouter>
+  );
 }
-
-export default App;
